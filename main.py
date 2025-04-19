c21f23d66bec811a17b5f4fc243e0c07c532c0f4
gechym <nguyenducbao166@gmail.com>
2025-04-19 22:59:04 +0700

    feat: Demo flow agent

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 import asyncio
 
-from agents import Runner, trace
+from agents import RawResponsesStreamEvent, Runner, TResponseInputItem, trace
+from openai.types.responses import ResponseContentPartDoneEvent, ResponseTextDeltaEvent
 
 from _setting import setup
-from mutil_agents.agents.manager_agent import create_manager_agent
+from mutil_agents.agents import create_event_agent, create_manager_agent, create_payment_support_agent
 
 
 async def main() -> None:
     setup()
-    manager_agent = create_manager_agent("gemini-2.0-flash")
+    manager_agent = create_manager_agent("gpt-4.1")
+    event_agent = create_event_agent("gpt-4.1")
+    payment_support_agent = create_payment_support_agent("gpt-4.1")
 
-    input_data = []
-    with trace("Demo chatbot agent"):
+    manager_agent.handoffs.append(event_agent)
+    manager_agent.handoffs.append(payment_support_agent)
+
+    event_agent.handoffs.append(manager_agent)
+    event_agent.handoffs.append(payment_support_agent)
+
+    payment_support_agent.handoffs.append(manager_agent)
+    payment_support_agent.handoffs.append(event_agent)
+
+    agent = manager_agent
+    msg = input("Hi!\n")
+    inputs: list[TResponseInputItem] = [{"content": msg, "role": "user"}]
+
+    with trace("Test Flow Agent"):
         while True:
+            result = Runner.run_streamed(
+                agent,
+                input=inputs,
+            )
+            async for event in result.stream_events():
+                if not isinstance(event, RawResponsesStreamEvent):
+                    continue
+                data = event.data
+                if isinstance(data, ResponseTextDeltaEvent):
+                    print(data.delta, end="", flush=True)
+                elif isinstance(data, ResponseContentPartDoneEvent):
+                    print("\n")
+
+            inputs = result.to_input_list()
+
             user_input = input("Enter a message: ")
-            input_data.append({
-                "role": "user",
-                "content": user_input,
-            })
-            result = await Runner.run(manager_agent, user_input)
-            print(result.final_output)
-            input_data = result.to_input_list()
+            inputs.append({"content": user_input, "role": "user"})
+            agent = result.current_agent
 
 
 if __name__ == "__main__":
--- mutil_agents/agents/__init__.py
+++ mutil_agents/agents/__init__.py
@@ -1,5 +1,5 @@
+from .event_agent import create_event_agent
 from .manager_agent import create_manager_agent
+from .payment_support_agent import create_payment_support_agent
 
-__all__ = [
-    "create_manager_agent",
-]
+__all__ = ["create_manager_agent", "create_payment_support_agent", "create_event_agent"]
